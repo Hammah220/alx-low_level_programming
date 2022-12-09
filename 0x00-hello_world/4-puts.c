d08be906263caf610cb1ed0c1982efa2c301c605
Hammah220 <live.yusif@gmail.com>
2022-12-09 01:37:42 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * main - Print a line of code using the function puts
+ *
+ * Return : returns 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
