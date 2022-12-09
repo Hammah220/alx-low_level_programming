066c78d5b0da61949395f92a7ac4f21d97d31caf
Hammah220 <live.yusif@gmail.com>
2022-12-09 02:36:09 -0800

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu bytes(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu bytes(S)\n", (unsigned long)sizeof(e));
-	return(0);
+	return (0);
 }
