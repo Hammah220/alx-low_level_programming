84a8829cb08276d87798e5b2cf8f6e5dc145372e
Hammah220 <live.yusif@gmail.com>
2022-12-12 14:13:09 -0800

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#inlcude < stdio.h >
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
