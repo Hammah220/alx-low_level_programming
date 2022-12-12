9884c062a6be4573419dab1a3f38f50f94683bcd
Hammah220 <live.yusif@gmail.com>
2022-12-12 14:48:45 -0800

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Prints combination of 2 digits
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones < 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
