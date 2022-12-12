5473afc90de47174ffc995fadf1ec4dde0fa048c
Hammah220 <live.yusif@gmail.com>
2022-12-12 14:33:03 -0800

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char c = 'a';
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
