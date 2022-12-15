1febf34c58249f80b7da0f0e71c545a76158a62c
Hammah220 <live.yusif@gmail.com>
2022-12-15 12:22:13 -0800

    new

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,22 +10,3 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
-
-Task 3. 3-print_numbers.c
-
-#include "main.h"
-
-/**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
- */
-void print_numbers(void)
-{
-	int ch;
-
-	for (ch = 48; ch < 58; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
-}
