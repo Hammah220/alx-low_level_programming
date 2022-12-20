aeadbd3b75d7b14d26bcf28a59f47914e0eb7dd6
Hammah220 <98092213+Hammah220@users.noreply.github.com>
2022-12-20 08:37:07 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,16 @@
  */
 void _puts(char *str)
 {
-    int count = 0;
-    
-    while (count >= 0)
-    {
-        if (str[count] == '\0')
-        {
-            _putchar('\n');
-            break;
-        }
-        _putchar(str[count]);
-        count++;
+	int count = 0;
+	
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
     }
 }
