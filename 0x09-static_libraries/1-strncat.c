ffc7f95d601534e98808451902a366682d84ec90
Hammah220 <98092213+Hammah220@users.noreply.github.com>
2023-01-14 21:28:05 +0000

    Update 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strncat - concatenate two strings, add inputted number of bytes
+*@dest: string to append
+*@src: string to complete at end of dest
+*@n:integer parameter
+*Return: void
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
+}
