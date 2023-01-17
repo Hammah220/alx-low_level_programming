3cfb758ed915f3445a96b7ffb0e4b0f6c7e6e105
Hammah220 <98092213+Hammah220@users.noreply.github.com>
2023-01-17 20:48:09 +0000

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_dog - Function that frees memory created in 4-new_dog.c
+* @d: Input struct
+*/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
