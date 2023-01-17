9c69a1a82c2e58796b9158d0bba91b78c881d537
Hammah220 <98092213+Hammah220@users.noreply.github.com>
2023-01-17 20:50:54 +0000

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Program that prints out the name of the file being compiled
+* @void: Nothing
+* Return: Success
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
