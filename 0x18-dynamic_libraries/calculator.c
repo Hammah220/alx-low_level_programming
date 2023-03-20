8ed1d01821ab115d8209dac8ab5f177fd0349423
Hammah220 <98092213+Hammah220@users.noreply.github.com>
2023-03-20 21:41:43 +0000

    Create calculator.c

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,64 @@
+/**
+ * add - Addition function.
+ *
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Substraction function.
+ *
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The difference of a and b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplication function.
+ *
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Division function.
+ *
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The division of a and b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Module function.
+ *
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The module of a and b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
